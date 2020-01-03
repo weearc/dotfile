dfd2b26264f5f69310c6926fe5e5ad9b87845ace
weearc <qby19981121@gmail.com>
2020-01-03 18:24:47 +0800

    add mpv thumbnail

--- tool.go
+++ tool.go
@@ -4,11 +4,36 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-
+	"log"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
 	"gopkg.in/urfave/cli.v1"
 )
 
+type conf struct {
+	Enabled bool   `yaml:"enabled"`
+  Path    string `yaml:"path"`
+}
+
+func (c *conf) getConf() *conf {
+
+    yamlFile, err := ioutil.ReadFile("conf.yaml")
+    if err != nil {
+        log.Printf("yamlFile.Get err   #%v ", err)
+    }
+    err = yaml.Unmarshal(yamlFile, c)
+    if err != nil {
+        log.Fatalf("Unmarshal: %v", err)
+    }
+
+    return c
+}
+
 func main() {
+
+	var c conf
+  c.getConf()
+
 	app := cli.NewApp()
 	app.Name = "Initialize tool"
 	app.Usage = "To set up a new system fast."
